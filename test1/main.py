faa5f3e750c7b832896366f0ac8acb384e895433
Dimkeee777 <lbtumatym1@gmail.com>
2023-01-05 01:09:42 +0300

    change readme.md

---
+++ test1/main.py
@@ -0,0 +1,11 @@
+#Comment
+#print("Result:",7, 15,'.', sep='', end='')
+#print('Second line')
+# print("Result:", 5 // 3)
+# print("Result:", max(5, 6, 7, -10, 12))
+
+# print("Result:", round(5 / 3))
+# print("Result:", (5 % 3))
+
+# input("ВВедите свой возраст: ")
+print(136 // 7)
